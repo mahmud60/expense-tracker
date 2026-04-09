c29b6f09c61a79c3ec1e12e118815319cebe1d65
mahmud60 <mahmud.hasan.igni@gmail.com>
2026-04-10 00:55:15 +0200

    CORS middleware added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List, Optional
 import models, schemas
